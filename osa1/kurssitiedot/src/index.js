21a52a3df62461b596c7dc0c74b39e7250163b00
ppedro <peetupa@metropolia.fi>
2019-03-16 00:48:06 +0200

    osa1 kurssitiedot step5

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -3,23 +3,26 @@ import ReactDOM from 'react-dom'
 
 
 const Header = (props) => {
+    console.log(props)
     return (
         <h1>{props.course}</h1>
     )
 }
 
 const Content = (props) => {
+    console.log(props);
     return (
         <div>
-            <Part part={props.part1} exer={props.exercises1} />
-            <Part part={props.part2} exer={props.exercises2} />
-            <Part part={props.part3} exer={props.exercises3} />
+            <Part part={props.parts[0]} />
+            <Part part={props.parts[1]} />
+            <Part part={props.parts[2]} />
         </div>
     )
 }
 const Part = (props) => {
+    console.log(props)
     return (
-        <p>{props.part} {props.exer}</p>
+        <p>{props.part.name} {props.part.exercises}</p>
     )
 }
 const Total = (data) => {
@@ -31,22 +34,22 @@ const Total = (data) => {
 
 
 const App = () => {
-  const course = 'Half Stack -sovelluskehitys'
-  const part1 = 'Reactin perusteet'
-  const exercises1 = 10
-  const part2 = 'Tiedonvälitys propseilla'
-  const exercises2 = 7
-  const part3 = 'Komponenttien tila'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3;
+      const course = {
+          name: 'Half Stack -sovelluskehitys',
+          parts: [
+              { name: 'Reactin perusteet', exercises: 10 },
+              { name: 'Tiedonvälitys propseilla', exercises: 7 },
+              { name: 'Komponenttien tila', exercises: 14  }
+          ]
+      }
+
+      const total = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
 
   return (
     <div>
-        <Header course={course} />
+        <Header course={course.name} />
 
-        <Content part1={part1} exercises1={exercises1}
-                 part2={part2} exercises2={exercises2}
-                 part3={part3} exercises3={exercises3}
+        <Content parts={course.parts}
         />
 
         <Total total={total} />
