e508efaf3fb7e04d72647923101d7b5efd75d911
ppedro <peetupa@metropolia.fi>
2019-03-18 17:55:24 +0200

    unicafe step6

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -3,15 +3,54 @@ import ReactDOM from 'react-dom'
 
 
 const Stats = ({ good, neutral, bad }) => {
-    return (
-        <div>
-            <h1>statistiikka</h1>
+    var total = good + neutral + bad;
+    var badVal = bad * -1;
+    var avg = (good + badVal) / total;
+    var posFB = (good / total) * 100;
 
-            <p>hyvä { good }</p>
-            <p>neutraali { neutral }</p>
-            <p>huono { bad }</p>
-        </div>
-    )
+    if (total > 0) {
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <h1>statistiikka</h1>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="left">hyvä</td>
+                            <td>{ good }</td>
+                        </tr>
+                        <tr>
+                            <td class="left">neutraali</td>
+                            <td>{ neutral }</td>
+                        </tr>
+                        <tr>
+                            <td class="left">huono</td>
+                            <td>{ bad }</td>
+                        </tr>
+                        <tr>
+                            <td class="left">yhteensä</td>
+                            <td>{ total }</td>
+                        </tr>
+                        <tr>
+                            <td class="left">keskiarvo</td>
+                            <td>{ avg }</td>
+                        </tr>
+                        <tr>
+                            <td class="left">positiivisia</td>
+                            <td>{ posFB } %</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <p>Yhtäkään palautetta ei ole annettu</p>
+            </div>
+        )
+    }
 }
 
 const Input = ({ incGood, incNeutral, incBad }) => {
@@ -33,7 +72,6 @@ const ReviewButton = ({ handler, text }) => {
     )
 }
 
-
 const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
@@ -41,7 +79,7 @@ const App = () => {
 
     const incGood = () => setGood(good + 1)
     const incNeutral = () => setNeutral(neutral + 1)
-    const incBad = () => setBad(neutral + 1)
+    const incBad = () => setBad(bad + 1)
 
     return (
         <>
